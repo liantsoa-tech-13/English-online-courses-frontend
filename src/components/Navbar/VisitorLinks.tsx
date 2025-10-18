b6083b6323b8c249a93f273bbf8713a2c9ca70e1
Liantsoa Tech 13 <ratovonanaharyliantsoa@gmail.com>
2025-10-18 15:44:10 +0300

    chore(navbar): added style

--- src/components/Navbar/VisitorLinks.tsx
+++ src/components/Navbar/VisitorLinks.tsx
@@ -1,19 +1,23 @@
+import styles from './Navbar.module.css'
+
 const VisitorLinks: React.FC = () => {
   return (
-    <div>
-      <div>
-        <img src="../../public/Logo/Logo.png" alt="Logo" />
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <img className={styles.logoImage} src="../../public/Logo/Logo.png" alt="Logo" />
+        <h3> EOC<span>M</span></h3>
       </div>
 
-      <ul>
+      <ul className={styles.navLinks}>
         <li><a href="/login">Home</a></li>
-        <li><a href="/register">Courses</a></li>
-        <li><a href="/register">Contact</a></li>
+        <li><a href="/contact">Pricing</a></li>
+        <li><a href="/courses">Courses</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
 
-      <div>
+      <div className={styles.authButtons}>
          <button><a href="/register">Register</a></button>
-         <button><a href="/login">Login</a></button>
+         <button className={styles.login}><a href="/login">Login</a></button>
       </div>
     </div>
   );
